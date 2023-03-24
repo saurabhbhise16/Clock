6d0c724420cd3a4462df61762f03aa89eaaa8a75
Saurabh Bhise <saurabhbhise16@live.com>
2023-03-24 22:37:16 +0530

    Final COmmit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+        // console.log("Hello");
+        // //document.write("This is document write") 
+        // console.warn("This is mwarmning")
+        // console.error("Thisis an error")
+var one = 2;
+var two = 4;
+var str1 = 'first string'
+var str2 = 'second string'
+// console.log(one + two);
+// console.log(str1 + str2);
+
+var marks = {
+    rav : 45,
+    dae : 334,
+    minte: 43
+}
+
+//operators in JS
